373e1293eed6b98d936d7b822e3b10605aa5f75b
Jorge Ribeiro <nicolas.jnr10@outlook.com>
2024-06-23 23:19:18 -0300

    ajusta app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const animalRoutes = require('./animal.routes');
-require('dotenv').config();  // Carrega as variáveis do arquivo .env
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Connect to MongoDB Atlas
 const mongoUri = process.env.MONGODB_URI;
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUri)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
